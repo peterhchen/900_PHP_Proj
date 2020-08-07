95a61fb7f84543c86038bf1ff83317ddb3e3901f
peterhchen <peter.hp.chen@gmail.com>
2020-08-06 20:30:47 -0700

    Add files via upload

---
+++ 04_variable/PHPproject/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<form method="GET">
+    <input type = "text" name="person">
+    <button>SUBMIT</button>
+</form>   
+<?php
+    if (empty($_GET['person'])) {
+        $name = "";
+    } else {
+        $name = $_GET['person'];
+    }
+    echo "name: " . $name;
+?>
+</body>
+</html>
