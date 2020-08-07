689d5b1395ee7a88d0e868b6d5765093e51339f6
peterhchen <peter.hp.chen@gmail.com>
2020-08-07 11:34:34 -0700

    Add files via upload

---
+++ 08_arithmetic-operation/PHPproject/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<?php
+    // Arithmetic Operator
+    echo 'Arithmetic Operator: <br>';
+    echo '5 + 7: ' . (5 + 7);
+    echo '<br>';
+    echo '5 - 7: ' . (5 - 7);
+    echo '<br>';
+    echo '5 * 7: ' . 5 * 7;
+    echo '<br>';
+    echo '5 / 7: ' . 5 / 7;
+    echo '<br>';
+    echo '5 ** 2: ' . 5 ** 2;
+?>
+</body>
