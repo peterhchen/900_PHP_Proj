022249444788e1d58c4e1215fdf37696eff65d9f
peterhchen <peter.hp.chen@gmail.com>
2020-08-06 17:00:42 -0700

    Add files via upload

---
+++ 02_setup-local-server/PHPproject/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body> 
+content of document...
+<br>   
+<?php
+    echo "Hi There ";
+    print "Peter";
+    echo "<br>";
+    echo "Basic math statement: ";
+    echo 10+5;
+?>
+</body>
+</html>
