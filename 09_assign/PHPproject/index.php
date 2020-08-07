48276deb944a093670408f69ee7f3fbac9323e19
peterhchen <peter.hp.chen@gmail.com>
2020-08-07 11:44:02 -0700

    Add files via upload

---
+++ 09_assign/PHPproject/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<?php
+    // Assignment Operator
+    echo 'Assignment Operator: <br>';
+    $x = 100;
+    echo '$x = 100: ' . $x;
+    echo '<br>';
+    $x += 20;
+    echo '$x += 100: ' . $x;
+    echo '<br>';
+    $x -= 20;
+    echo '$x -= 100: ' . $x;
+    echo '<br>';
+    $x *= 20;
+    echo '$x *= 100: ' . $x;
+    echo '<br>';
+    $x /= 20;
+    echo '$x /= 100: ' . $x;
+    echo '<br>';
+    $x %= 20;
+    echo '$x %= 100: ' . $x;
+    echo '<br>';
+?>
+</body>
