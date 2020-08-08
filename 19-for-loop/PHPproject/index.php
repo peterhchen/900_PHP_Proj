69c7f7b4467863ed79802dc3869ffa027af8b204
peterhchen <peter.hp.chen@gmail.com>
2020-08-08 10:13:51 -0700

    Add files via upload

---
+++ 19-for-loop/PHPproject/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+    // Loop:
+    // do while Loop
+    // for loop
+    // foreach loop
+    echo ("for loop<br>");
+    for ($x = 0; $x < 5; $x++) {
+        echo "Hi There <br>";
+    }
+?>
+</body>
+</html>
