8b340b50ce916722be5dcfbf3ac7ffa303a5522a
peterhchen <peter.hp.chen@gmail.com>
2020-08-08 11:08:25 -0700

    Add files via upload

---
+++ 23_include-doc/PHPproject/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+    <!-- <h1> Header.php </h1> -->
+    <nav>
+        <ul>
+            <li><a href="">nav1</a></li>
+            <li><a href="">nav2</a></li>
+            <li><a href="">nav3</a></li>
+        </ul>
+    </nav>    
+</header>
---
+++ 23_include-doc/PHPproject/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h3> include 'header.php' </h3>
+<?php
+    include 'header.php'
+?>
+
+<section>
+    <div class = "Main Content">
+        <h1>Hi There</h1>
+    </div>
+</section>
+</body>
+</html>
