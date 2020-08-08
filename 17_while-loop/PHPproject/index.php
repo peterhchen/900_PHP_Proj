37eca489f31c96e5b8e6817b0ab0d00f786b5bbc
peterhchen <peter.hp.chen@gmail.com>
2020-08-08 09:45:30 -0700

    Add files via upload

---
+++ 17_while-loop/PHPproject/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+    // Loop:
+    // do while Loop
+    // for loop
+    // foreach loop
+    $x = 1;
+    while ($x < 5) {
+        echo ("Hi There<br>");
+        $x++;
+    }
+
+?>
+</body>
+</html>
