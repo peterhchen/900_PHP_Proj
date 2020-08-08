8c3de8984408c5cd166a57c63bcfc5a61a96c6f5
peterhchen <peter.hp.chen@gmail.com>
2020-08-07 17:14:54 -0700

    Add files via upload

---
+++ 14_switch/PHPproject/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<?php
+    // Conditional Statement: if else ...
+    $x = 1;
+    if ($x == 1) {
+        echo 'if ($x == 1)';
+    } elseif ($x == 2) {
+        echo 'elseif ($x == 2)';
+    } else {
+        echo 'else:';
+    }
+?>
+</body>
