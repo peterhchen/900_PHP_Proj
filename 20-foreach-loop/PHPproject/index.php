cfd3ccc6e853ab072b3bc6d9e9831fb79c3b83f5
peterhchen <peter.hp.chen@gmail.com>
2020-08-08 10:22:49 -0700

    Add files via upload

---
+++ 20-foreach-loop/PHPproject/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+    // Loop:
+    // do while Loop
+    // for loop
+    // foreach loop
+    echo ("foreach loop<br>");
+    $array = array ("Peter", "Jason", "Jonathan");
+    foreach ($array as $x) {
+        echo "foreach loop: " . $x. "<br>";
+    }
+?>
+</body>
+</html>
